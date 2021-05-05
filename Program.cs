e37be6f4ab27653356c758d398e2d025005ecd9d
mzimasimbongwe <mzimasimbongwe98@gmail.com>
2021-05-05 12:07:27 +0200

    GitHub Test2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MzimasiGittApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Greetings from Mzimasi!");
+        }
+    }
+}
